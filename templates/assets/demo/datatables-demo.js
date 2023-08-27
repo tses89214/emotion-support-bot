51f08f034ec26b117ce26b1ef3651689d760342c
Chia-Xuan Jian <tses89214@gmail.com>
2023-08-27 15:41:53 +0800

    revise orders by

--- templates/assets/demo/datatables-demo.js
+++ templates/assets/demo/datatables-demo.js
@@ -1,6 +1,6 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function(activity_result) {
   $('#dataTable').DataTable({
-    "order": [[ 3, "desc" ]]
+    "order": [[ 0, "desc" ]]
   });
 });
